657bfd89302c38a44a46eb9ea1d7f524c5ffdca1
thehxdev <hossein.khosravi.ce@gmail.com>
2024-11-21 17:24:25 +0330

    refactor: mark functions as static
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- src/main.c
+++ src/main.c
@@ -93,7 +93,7 @@ HANDLE hStdin = INVALID_HANDLE_VALUE;
 DWORD fdwMode, fdwOldMode;
 #endif
 
-void disable_input_buffering() {
+static void disable_input_buffering(void) {
 #ifdef LC3_PLAT_UNIX
     tcgetattr(STDIN_FILENO, &original_tio);
     struct termios new_tio = original_tio;
@@ -110,7 +110,7 @@ void disable_input_buffering() {
 #endif
 }
 
-void restore_input_buffering() {
+static void restore_input_buffering(void) {
 #ifdef LC3_PLAT_UNIX
     tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
 #else
@@ -118,7 +118,7 @@ void restore_input_buffering() {
 #endif
 }
 
-uint16_t check_key() {
+static uint16_t check_key(void) {
 #ifdef LC3_PLAT_UNIX
     fd_set readfds;
     FD_ZERO(&readfds);
@@ -133,7 +133,7 @@ uint16_t check_key() {
 #endif
 }
 
-uint16_t mem_read(uint16_t addr) {
+static uint16_t mem_read(uint16_t addr) {
     if (addr == MR_KBSR) {
         if (check_key()) {
             memory[MR_KBSR] = (1 << 15);
@@ -145,18 +145,18 @@ uint16_t mem_read(uint16_t addr) {
     return memory[addr];
 }
 
-void mem_write(uint16_t addr, uint16_t value) {
+static void mem_write(uint16_t addr, uint16_t value) {
     memory[addr] = value;
 }
 
-uint16_t sign_extend(uint16_t num, int bit_count) {
+static uint16_t sign_extend(uint16_t num, int bit_count) {
     if ((num >> (bit_count-1)) & 0x1) {
         num |= (0xffff << bit_count);
     }
     return num;
 }
 
-void update_flags(uint16_t r) {
+static void update_flags(uint16_t r) {
     if (reg[r] == 0)
         reg[R_COND] = FL_ZRO;
     else if (reg[r] >> 15)
@@ -165,11 +165,11 @@ void update_flags(uint16_t r) {
         reg[R_COND] = FL_POS;
 }
 
-uint16_t swap16(uint16_t x) {
+static uint16_t swap16(uint16_t x) {
     return ((x << 8) | (x >> 8));
 }
 
-void read_image_file(FILE *fp) {
+static void read_image_file(FILE *fp) {
     uint16_t origin;
     fread(&origin, sizeof(origin), 1, fp);
     origin = swap16(origin);
@@ -185,7 +185,7 @@ void read_image_file(FILE *fp) {
     }
 }
 
-int read_image(const char path[]) {
+static int read_image(const char path[]) {
     FILE *fp = fopen(path, "rb");
     if (!fp)
         return 0;
@@ -194,11 +194,11 @@ int read_image(const char path[]) {
     return 1;
 }
 
-void shutdown() {
+static void shutdown(void) {
     restore_input_buffering();
 }
 
-void handle_interrupt(int signal) {
+static void handle_interrupt(int signal) {
     (void)signal;
 
     shutdown();
